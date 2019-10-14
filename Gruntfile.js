01e4586592c43a6586a7cfa694d037d4170acbf3
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-10-14 13:20:40 +0200

    Add bookmarkets

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+const path = require('path');
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-bookmarklet-wrapper');
+  grunt.loadNpmTasks('grunt-html-build');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    htmlbuild: {
+      dist: {
+        src: 'src/index.html',
+        dest: '',
+      },
+    },
+  });
+
+  grunt.registerTask('bookmarklet_preprocessor', '', function() {
+    const files = grunt.file.expand({ filter: "isFile", cwd: "src" }, ["*.js"]);
+    let bookmarklets = {};
+    let bw_files = {};
+
+    files.forEach(function (filename) {
+      const label = path.basename(filename, '.js');
+      bookmarklets[label] = {
+        target: path.join('tmp', filename),
+        source: path.join('src', filename)
+      };
+      bw_files[bookmarklets[label]['target']] = bookmarklets[label]['source'];
+    });
+
+    grunt.config.set(['bookmarklets'], bookmarklets);
+    grunt.config.set(['bookmarklet_wrapper', 'dist', 'files'], bw_files);
+  });
+
+  grunt.registerTask('bookmarklet_processor', '', function() {
+    links = [];
+    const isMarkdown = path.extname(grunt.config.get(['htmlbuild', 'dist', 'src'])).toLowerCase() === '.md';
+    Object.keys(grunt.config.get(['bookmarklets'])).forEach(function (label) {
+      const filecontent = grunt.file.read(grunt.config.get(['bookmarklets'])[label]['target']);
+      links.push(
+        isMarkdown
+          ? '<a href="' + filecontent + '">' + label + '</a>'
+          : '[' + label + '](' + filecontent + ')'
+      );
+    });
+
+    if (links.length) {
+      if (isMarkdown) {
+        grunt.config.set(['htmlbuild', 'dist', 'options', 'data', 'links'], ' * ' + links.join('\n\n * '));
+      }
+      else {
+        grunt.config.set(['htmlbuild', 'dist', 'options', 'data', 'links'], '<ul><li>' + links.join('</li><li>') + '</li></ul>');
+      }
+    }
+  });
+
+
+  grunt.registerTask('default', ['bookmarklet_preprocessor', 'bookmarklet_wrapper', 'bookmarklet_processor', 'htmlbuild']);
+};
---
+++ src/WCAG SC 1.4.12 – Text Spacing with 2x root font size.js
@@ -0,0 +1,43 @@
+var elems = document.querySelectorAll('*');
+var classIsInClassString = function (className, classValue) {
+  return classValue.split(' ').filter(function (item) {
+    return item;
+  }).indexOf(className.trim()) > -1;
+};
+
+document.querySelector('html').style.setProperty('font-size', '200%');
+
+for (let index = 0; index < elems.length; index++) {
+  var el = elems[index];
+
+  // Add properties based on element node name.
+  var tag = ['p', 'li', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+  if (tag.indexOf(el.nodeName.toLowerCase()) > -1) {
+    el.style.setProperty('margin-bottom', '2em', 'important');
+  }
+  else {
+    // Add props based on tag AND a CSS class.
+    var tagAndClass = {
+      'div': [
+        'form-item__description',
+        'form-item__error-message',
+        'fieldset__description',
+        'fieldset__error-message',
+        'messages'
+      ]
+    };
+    Object.keys(tagAndClass).forEach(function (tagAndClassTag) {
+      if (tagAndClassTag.indexOf(el.nodeName.toLowerCase()) > -1) {
+        tagAndClass[tagAndClassTag].forEach(function (classToFind) {
+          if (classIsInClassString(classToFind, el.classList.value)) {
+            el.style.setProperty('margin-bottom', '4em', 'important');
+          }
+        });
+      }
+    });
+  }
+
+  el.style.setProperty('line-height', '1.5', 'important');
+  el.style.setProperty('letter-spacing', '0.12em', 'important');
+  el.style.setProperty('word-spacing', '0.16em', 'important');
+}
